60b6e085cdcd32b33026e58368f2e9a5527220e8
Mark Scala <markscala@gmail.com>
2014-10-10 08:12:32 -0500

    form for comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: :destroy
+  resources :posts, except: :destroy do
+    resources :categories, only: :create
+  end
+
   resources :categories, only: [:new, :create, :show]
 end
