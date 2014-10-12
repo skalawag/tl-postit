23c9e61f2ebc1e80c2fc48048a563b49c7c81dcd
Mark Scala <markscala@gmail.com>
2014-10-12 11:58:00 -0500

    db schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011213042) do
+ActiveRecord::Schema.define(version: 20141012145148) do
 
   create_table "categories", force: true do |t|
     t.string "category"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141011213042) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.text     "bio"
   end
 
 end
