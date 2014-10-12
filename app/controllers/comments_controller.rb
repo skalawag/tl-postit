368048a560f5a6f531591d56a89063feaf29849b
Mark Scala <markscala@gmail.com>
2014-10-12 07:57:16 -0500

    only allow logged in users to create a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,22 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(params.require(:comment).permit(:body))
-    ## prev line equivalent to:
-    #@comment = Comment.new(params.require(:comment).permit(:body))
-    #@comment.post = @post
-    @comment.user_id = current_user
-    if @comment.save
-      flash[:notice] = "Your comment has been saved."
-      redirect_to post_path(@post)
+    if not logged_in?
+      flash[:error] = "You must be logged in to leave a comment."
+      redirect_to post_path(params[:post_id])
     else
-      render '/posts/show'
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.build(params.require(:comment).permit(:body))
+      ## prev line equivalent to:
+      #@comment = Comment.new(params.require(:comment).permit(:body))
+      #@comment.post = @post
+      @comment.user_id = current_user.id
+      if @comment.save
+        flash[:notice] = "Your comment has been saved."
+        redirect_to post_path(@post)
+      else
+        render '/posts/show'
+      end
     end
   end
 end
