1d38315cf1419cb93e2842b85cf5cfb0fc92a9b4
Mark Scala <markscala@gmail.com>
2014-10-12 11:55:52 -0500

    show edit update profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = current_user
+    if user.update(user_params)
+      flash[:notice] = "Your profile has been updated."
+      redirect_to users_profile_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :bio)
   end
 end
