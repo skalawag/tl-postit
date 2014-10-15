9b704deda422fdc2c02a20f63da2073d0c35ca4f
Mark Scala <markscala@gmail.com>
2014-10-15 12:50:04 -0500

    validate title for uniqueness, for unique slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,9 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :votes, as: :votable
 
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, uniqueness: true, length: {minimum: 5}
   validates :description, presence: true
-  validates :url, presence: true, uniqueness: true
-  validates :url, format: {with: /\A((http|https):\/\/)?(([a-z0-9-\.]*)\.)?([a-z0-9-]+)\.([a-z]{2,5})(:[0-9]{1,5})?(\/)?\z/ix}
+  validates :url, format: {with: /\A\z|\A((http|https):\/\/)?(([a-z0-9-\.]*)\.)?([a-z0-9-]+)\.([a-z]{2,5})(:[0-9]{1,5})?(\/)?\z/ix}
 
   before_save :generate_slug
 
