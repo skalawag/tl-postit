45bd548a12cf73896c1a7313153f5511890b8a01
Mark Scala <markscala@gmail.com>
2014-10-24 20:46:27 -0500

    fixed url validation regex

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true, uniqueness: true, length: {minimum: 5}
   validates :description, presence: true
-  validates :url, format: {with: /\A\z|\A((http|https):\/\/)?(([a-z0-9-\.]*)\.)?([a-z0-9-]+)\.([a-z]{2,5})(:[0-9]{1,5})?(\/)?\z/ix}
+  validates :url, format: {with: /\A(http:|https:)\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix}
 
   sluggable_column :title
   before_save :generate_slug
